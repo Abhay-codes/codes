50ee9a35781c4d8eb8a7a8c46841e69438df75a0
abhaysingh00 <abhay.2024cse1147@kiet.edu>
2022-01-10 20:57:12 +0530

    problem solving

---
+++ problem solving/solve me first .c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int solveMeFirst(int a, int b) {
+   
+ int s= a+b;
+ return s;
+}
+
+int main() {
+    int num1,num2;
+    scanf("%d %d",&num1,&num2);
+    int sum; 
+    sum = solveMeFirst(num1,num2);
+    printf("%d",sum);
+    return 0;
+}
